43b4683c41423413bf2509a2d5aa2fde405aac06
Julio César <166463949+Fluxo-Juridico@users.noreply.github.com>
2026-09-23 18:51:09 -0300

    refactor(vercel): remove individual internal function wrapper

--- api/internal/billing-export.js
+++
@@ -1,4 +0,0 @@
-import handler from "../../server/api/internal/billing-export.js";
-import {wrapHandler} from "../../server/lib/http.js";
-export * from "../../server/api/internal/billing-export.js";
-export default wrapHandler(handler,{service:"fluxo-juridico-vendas"});
