2f25763d6857c810f7a4dcca53c99ee38758aa72
Julio César <166463949+Fluxo-Juridico@users.noreply.github.com>
2026-09-23 18:40:33 -0300

    fix(vercel): register privacy function at source time

---
+++ api/privacy.js
@@ -0,0 +1,4 @@
+import handler from "../server/api/privacy.js";
+import {wrapHandler} from "../server/lib/http.js";
+export * from "../server/api/privacy.js";
+export default wrapHandler(handler,{service:"fluxo-juridico-vendas"});
