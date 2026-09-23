a901570d417ac7b6dbfc53b5d317120906b2af4e
Julio César <166463949+Fluxo-Juridico@users.noreply.github.com>
2026-09-23 18:51:12 -0300

    refactor(vercel): remove individual internal function wrapper

--- api/internal/crm-export.js
+++
@@ -1,4 +0,0 @@
-import handler from "../../server/api/internal/crm-export.js";
-import {wrapHandler} from "../../server/lib/http.js";
-export * from "../../server/api/internal/crm-export.js";
-export default wrapHandler(handler,{service:"fluxo-juridico-vendas"});
