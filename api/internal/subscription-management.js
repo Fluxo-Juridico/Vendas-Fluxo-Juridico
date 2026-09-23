9c5b9d8209519950cc75a472d7ab90d57e599d51
Julio César <166463949+Fluxo-Juridico@users.noreply.github.com>
2026-09-23 18:51:18 -0300

    refactor(vercel): remove individual internal function wrapper

--- api/internal/subscription-management.js
+++
@@ -1,4 +0,0 @@
-import * as server from "../../server/api/internal/subscription-management.js";
-import { adaptServerRoute } from "../../platform/route-adapter.js";
-export const maxDuration = 60;
-export default adaptServerRoute(server);
